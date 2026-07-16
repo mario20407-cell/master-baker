0a7fc60d41ef9262d81deabf4f887d0d382c9f38
mario20407-cell <mario20407@gmail.com>
2026-07-16 09:09:59 -0600

    feat: PWA instalable del panel de fundadores

---
+++ frontend/public/panel-fundadores/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'panel-fundadores-v1';
+const SHELL = ['/panel-fundadores/', '/panel-fundadores/index.html'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(() => {}));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.startsWith('/api/')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
